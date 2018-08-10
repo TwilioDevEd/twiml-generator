2eafd2abfb386da518a727e5c0d2214219af5a26
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-08-10 14:27:45 +0200

    Change attribute_name_style on Node to underscore

--- twiml_generator/twiml_code_generator.py
+++ twiml_generator/twiml_code_generator.py
@@ -315,7 +315,7 @@ class TwimlCodeGenerator(object):
             else:
                 value = repr(value)
             # The Node interpret-as parameter needs to be surrounded by quotes, since it uses a dash
-            if self.language_spec.get('language') == 'node' and name == 'interpret_as':
+            if self.language_spec.get('language') == 'node' and name == 'interpretAs':
                 name = "'interpret-as'"
 
             built_attributes.append(self.language_spec['attribute_format'].format(name=name, value=value))
