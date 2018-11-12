d66b6eb1d2c8d7794f556599de59e32961b0bf15
Maylon Pedroso <mpedroso@stackbuilders.com>
2018-11-12 18:15:57 -0500

    Fix PHP specs and specificities

--- twiml_generator/specificity/php.py
+++ twiml_generator/specificity/php.py
@@ -2,9 +2,38 @@ from twiml_generator.specificity.common import Language
 
 
 class PHP(Language):
+    _classes = {}
 
     @classmethod
     def clean(cls, generator):
         for verb, event in generator.twimlir:
             if verb.name == 'break':
                 verb.name = 'break_'
+
+            cls.verb_processing(verb, generator.specific_imports)
+
+
+class DefaultText:
+
+    @classmethod
+    def process(cls, verb, imports):
+        if not verb.text:
+            verb.text = ' '
+
+
+@PHP.register
+class Message(DefaultText):
+    @classmethod
+    def process(cls, verb, imports):
+        if not verb.text:
+            verb.text = ' '
+
+
+@PHP.register
+class Dial(DefaultText):
+    pass
+
+
+@PHP.register
+class Play(DefaultText):
+    pass
